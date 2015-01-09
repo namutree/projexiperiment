4ad88f0054eb61090f8e1e314145275f75f1b1d2
John N Choi <jnc331@nyu.edu>
2015-01-09 17:22:57 +0900

    first commit

---
+++ xcode/NemoCon.cpp
@@ -0,0 +1,9 @@
+//
+//  NemoCon.cpp
+//  projexiperiment01
+//
+//  Created by john namu choi on 1/7/15.
+//
+//
+
+#include "NemoCon.h"
---
+++ xcode/NemoCon.h
@@ -0,0 +1,14 @@
+//
+//  NemoCon.h
+//  projexiperiment01
+//
+//  Created by john namu choi on 1/7/15.
+//
+//
+
+#ifndef __projexiperiment01__NemoCon__
+#define __projexiperiment01__NemoCon__
+
+#include <stdio.h>
+
+#endif /* defined(__projexiperiment01__NemoCon__) */
